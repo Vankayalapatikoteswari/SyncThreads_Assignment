0cb08cf9cf2cf363073840549f113657a941d79e
Vankayalapatikoteswari <166218976+Vankayalapatikoteswari@users.noreply.github.com>
2025-03-25 10:55:18 +0530

    main.jsx

--- main.jsx
+++ main.jsx
@@ -1,1 +1,21 @@
+mport React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import './index.css'
 
+import {AuthContextProvider} from './components/Context/AuthContext'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+  <React.StrictMode>
+    <AuthContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthContextProvider>
+  </React.StrictMode>
+
+
+
+)
