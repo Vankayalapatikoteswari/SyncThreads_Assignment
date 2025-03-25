7f1b5cade07573774643b75ed871da9c358caa07
Vankayalapatikoteswari <166218976+Vankayalapatikoteswari@users.noreply.github.com>
2025-03-25 10:54:03 +0530

    App.jsx

--- App.jsx
+++ App.jsx
@@ -1,1 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import AllRoutes from './components/Routes/AllRoutes'
 
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+     <AllRoutes></AllRoutes>
+    </div>
+  )
+}
+
+export default App
